2ef81d56ca5394fdc8e02e4be7a8a7f4fa58d609
padlisurya <79122347+padlisurya@users.noreply.github.com>
2022-12-05 23:26:32 +0700

    Update info-sc.cjs

--- plugins/info-sc.cjs
+++ plugins/info-sc.cjs
@@ -1,7 +1,7 @@
 var handler = async(m, { conn, text, usedPrefix, command }) => {
 
-var str = `*https://github.com/FahriAdison/Sad-Bot-V2*\nSubscribe Untuk Mengetahui Update Selanjutnya:bit.ly/Papah-Chan\nSilahkan follow github saya dibawah ,Terimakasih`
-conn.sendButtonDoc(m.chat, str, wm, 'Thanks','Bilek', ftextt, m)
+var str = `\n\nhttps://github.com/FahriAdison/Sad-Bot-V2`
+conn.sendButtonDoc(m.chat, str, wm, 'Oke','Bilek', ftextt, m)
 }
 handler.command = handler.help = ['sc']
 handler.tags = ['info']
