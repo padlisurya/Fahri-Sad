1cb3c98d78ec26ea7c73d6c7dcec352af4a14b98
padlisurya <79122347+padlisurya@users.noreply.github.com>
2022-12-05 17:27:42 +0700

    Update user-simimi.cjs

--- plugins/user-simimi.cjs
+++ plugins/user-simimi.cjs
@@ -2,7 +2,7 @@ var handler = async (m, {
 	conn,
 	text
 }) => {
-	if (!text) throw 'Hai!!, saya robot. ada yang bisa saya bantu?. ketik #menu untuk melihat fitur🗿'
+	if (!text) throw 'Hai, saya bot WhatsApp, silahkan ketik *#menu*'
 	try {
 		a = (await axios.get(API('ghst', 'api/simsimi', {
 			pesan: text
