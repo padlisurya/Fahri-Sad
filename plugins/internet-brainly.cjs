9d1fdac4cf38bc8bc7a5507dd7546e776412ed29
padlisurya <79122347+padlisurya@users.noreply.github.com>
2022-12-06 10:04:25 +0700

    Update internet-brainly.cjs

--- plugins/internet-brainly.cjs
+++ plugins/internet-brainly.cjs
@@ -2,12 +2,12 @@ var brainly = require('brainly-scraper-v2')
 var handler = async function(m, {
 	text
 }) {
-	if (!text) throw 'Silahkan ketik .br (soalnya apaa)'
+	if (!text) throw '.brainly (soalnya apaa)'
 	var res = await brainly(text)
 	var answer = res.data.map((v, i) => `_*PERTANYAAN KE ${i + 1}*_\n${v.pertanyaan}\n${v.jawaban.map((v,i) => `*JAWABAN KE ${i + 1}*\n${v.text}`).join('\n')}`).join('\n\n•------------•\n\n')
 	m.reply(answer)
 }
-handler.help = ['br [soal]']
+handler.help = ['brainly [soal]']
 handler.tags = ['internet']
 handler.command = /^brainly$/i
 handler.limit = true
