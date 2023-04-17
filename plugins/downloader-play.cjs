bc55c68c609f2730de7e4b2097b0b886da77dad9
padlisurya <79122347+padlisurya@users.noreply.github.com>
2023-04-18 02:41:42 +0700

    Update downloader-play.cjs

--- plugins/downloader-play.cjs
+++ plugins/downloader-play.cjs
@@ -1,28 +1,52 @@
 var { youtubeSearch } = require ('@bochilteam/scraper')
+
 var handler = async (m, { conn, command, text, usedPrefix }) => {
+
   if (!text) throw `Use example ${usedPrefix}${command} Minecraft`
+
   var vid = (await youtubeSearch(text)).video[0]
+
   if (!vid) throw 'Video/Audio Tidak ditemukan'
+
   var { title, description, thumbnail, videoId, durationH, viewH, publishedTime } = vid
+
   var url = 'https://www.youtube.com/watch?v=' + videoId
+
   await conn.sendHydrated(m.chat, `
+
 📌 *Title:* ${title}
+
 🔗 *Url:* ${url}
+
 🖹 *Description:* ${description}
+
 ⏲️ *Published:* ${publishedTime}
+
 ⌚ *Duration:* ${durationH}
+
 👁️ *Views:* ${viewH}
+
   `.trim(), author, thumbnail + '.png', url, '📺Go To Youtube!', null, null, [
+
     ['Audio 🎧', `${usedPrefix}getaud ${url} yes`],
+
     ['Video 🎥', `${usedPrefix}ytv ${url} yes`],
+
     ['Youtube Search🔎', `${usedPrefix}yts ${url}`]
+
   ], m, { viewOnce: true })
+
 }
+
 handler.help = ['play'].map(v => v + ' <pencarian>')
+
 handler.tags = ['downloader']
+
 handler.command = /^play?$/i
 
 handler.exp = 0
+
 handler.limit = false
 
 module.exports = handler
+
